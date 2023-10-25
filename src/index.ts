9eb3151c229c9082ee4c26b0f75e694d43b528b0
lian-0404 <lianna.khachatryan@softconstruct.com>
2023-10-25 17:47:28 +0400

    first commit

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { Sorter } from "./Sorter";
+import { NumberCollection } from "./NumberCollection";
+
+const numberCollection =new NumberCollection([10, 2000, -5, 0]);
+const sorter = new Sorter(numberCollection);
+sorter.sort();
+console.log(sorter.collection);
+
