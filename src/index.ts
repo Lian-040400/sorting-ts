8a2fc02cd2815545ddb72f0d74c0bd632a89e668
lian-0404 <lianna.khachatryan@softconstruct.com>
2023-10-26 17:17:24 +0400

    done

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
-import { Sorter } from "./Sorter";
 import { NumberCollection } from "./NumberCollection";
+import { CharacterCollection } from "./CharacterCollection";
+import { LinkedList } from './LinkedList';
 
-const numberCollection =new NumberCollection([10, 2000, -5, 0]);
-const sorter = new Sorter(numberCollection);
-sorter.sort();
-console.log(sorter.collection);
 
+const numbersCollection = new NumberCollection([50,3,-5,0]);
+numbersCollection.sort();
+console.log(numbersCollection.data);
+
+const charactersCollection = new CharacterCollection("Xaayb");
+charactersCollection.sort();
+console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+linkedList.sort();
+linkedList.print();
